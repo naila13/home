6019f3577787f002870663eec05f949b3fa83178
Naila Bushra <naila_cse.buet@yahoo.com>
2020-04-16 01:56:28 -0500

    test map1 2

--- map1/map1.js
+++ map1/map1.js
@@ -1,5 +1,5 @@
 var map1obj = L.map('map1_id').setView([32.18, -99.14], 4)
-L.tileLayer('map1-basemap-url').addTo(map1obj)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map1obj)
 var map1MarkerObject = L.marker([30, -90]).addTo(map1obj)
 var map1PolygonObject = L.polygon([
   [31, -88],
@@ -8,4 +8,3 @@ var map1PolygonObject = L.polygon([
 ]).addTo(map1obj)
 map1PolygonObject.bindPopup('Polygon popup text')
 map1PolygonObject.bindPopup('Marker popup text')
-
