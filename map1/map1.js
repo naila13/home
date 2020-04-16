3d4e6aa078b07c157cfaf029c3396847ba6fc653
Naila Bushra <naila_cse.buet@yahoo.com>
2020-04-16 01:50:07 -0500

    test map1 1

--- map1/map1.js
+++ map1/map1.js
@@ -1,12 +1,11 @@
-var map1obj = L.map('mymap1').setView([32.18, -99.14], 4)
-
+var map1obj = L.map('map1_id').setView([32.18, -99.14], 4)
 L.tileLayer('map1-basemap-url').addTo(map1obj)
-
 var map1MarkerObject = L.marker([30, -90]).addTo(map1obj)
-
 var map1PolygonObject = L.polygon([
   [31, -88],
   [35, -88],
   [32, -82]
 ]).addTo(map1obj)
+map1PolygonObject.bindPopup('Polygon popup text')
+map1PolygonObject.bindPopup('Marker popup text')
 
