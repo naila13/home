31380a275d3a5a53c5251db18b1353951e35a724
Naila Bushra <naila_cse.buet@yahoo.com>
2020-04-20 16:33:28 -0500

    map2 test 4

--- map2/map2.js
+++ map2/map2.js
@@ -1,4 +1,4 @@
-var map2obj = L.map('map2id').setView([32.18, -99.14], 40)
+var map2obj = L.map('map2id').setView([42.18, -93.14], 20)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map2obj)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
