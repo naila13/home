5a1d7502c7966945471d35e0ac9dcb9a63149781
Naila Bushra <naila_cse.buet@yahoo.com>
2020-04-21 00:57:08 -0500

    map3 test 1

--- map3/map3.js
+++ map3/map3.js
@@ -1,10 +1,10 @@
-var map1obj = L.map('map1id').setView([32.18, -99.14], 4)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map1obj)
-var map1MarkerObject = L.marker([30, -90]).addTo(map1obj)
-var map1PolygonObject = L.polygon([
+var map3obj = L.map('map3id').setView([32.18, -99.14], 4)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map3obj)
+var map3MarkerObject = L.marker([30, -90]).addTo(map3obj)
+var map3PolygonObject = L.polygon([
   [31, -88],
   [35, -88],
   [32, -82]
-]).addTo(map1obj)
-map1PolygonObject.bindPopup('Polygonpopuptext')
-map1PolygonObject.bindPopup('Markerpopuptext')
+]).addTo(map3obj)
+map3PolygonObject.bindPopup('Polygonpopuptext')
+map3PolygonObject.bindPopup('Markerpopuptext')
